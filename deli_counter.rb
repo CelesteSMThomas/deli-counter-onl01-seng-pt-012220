1db9328a91b49635dc7c9d285729f0d3a00fab8d
Celeste Thomas <celestesmthomas@gmail.com>
2020-01-31 05:30:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def line(katz_deli)
+if katz_deli.length == 0 
+    puts "The line is currently empty."
+elsif katz_deli.length > 0
+  order = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+ order += " #{index+1}. #{name}"
+end
+puts order
+end
+end
+
+
+def take_a_number (katz_deli, name)
+  katz_deli.each do |name| 
+    if katz_deli == 0 
+     katz_deli << name
+    elsif katz_deli > 0 
+      katz_deli << name
+  end
+    puts "Welcome, #{name}. You are number #"
+  end
+end
+
+
+
+def now_serving
+  
+end
+  
