82265acaf45d7d85ff36bca9a2f103eb21f91996
Celeste Thomas <celestesmthomas@gmail.com>
2020-01-31 05:34:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,11 +18,13 @@ end
 
 
 
-def now_serving
-  if now_serving.length == 0 
-    puts "There is nobody waiting to be served!"
-  elsif now_serving.length > 1 
-  
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts"There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
+    katz.shift
+  end
 end
   
   
